6975aab74f9f7fc52300b4c99be2b5335eebcf56
Caleb Wheeler <Cayj15@gmail.com>
2020-07-19 20:17:46 -0700

    Added GitIgnore

--- iTrackTV/UI/ContentView.swift
+++ iTrackTV/UI/ContentView.swift
@@ -5,10 +5,7 @@
 //  Created by Caleb Wheeler on 7/5/20.
 //  Copyright © 2020 Caleb Wheeler. All rights reserved.
 //
-// JWT TOken = eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTQ1OTY2MzMsImlkIjoiaVRyYWNrVFYiLCJvcmlnX2lhdCI6MTU5Mzk5MTgzMywidXNlcmlkIjoyMjg3MzkxLCJ1c2VybmFtZSI6IkNhbGViVGhlUGVyc29uIn0.hamqZAqp_ZIXAbsbq98xN_k5snx_fN8nrawClU4LIyewjMRqzQOHoMBmsZRcYOA-ESWhJwHf-34EsdpWS2ux6w8OmIVUAgjyRiDOv-4wWPGAWZ2n7uE3_e-gCD-UXdNyo2erB5ljvfG7nkagzYftP2392LkcWvRdTqbUeJ47u0xG3cwJezea7Bkr6XqEG-Ih4ONexoOGL7lBAS1PNy0F_xLxzSu-A9WWc7KiN-oN4VIJ78OL_gh2OEhpvxpa1Nq2Y_5g3GRPSD3KD4ffBKfmdLqHMqWdBNMrRUnynelmn-heNg6kk5Sj-wdze02c9h2llO2U5hNxVKA7-DX5Hxo8Pw
 
-// user key - 5ED05945A9A4E8.97984150
-// api key - a588d1f036c267171ba06a3af627fed9
 //Option command P
 
 
@@ -67,6 +64,9 @@ struct ShowView: View {
                 .aspectRatio(contentMode: .fill)
                 .frame(width: ScreenSize.width - 14, height: 107)
                 .clipped()
+            
+            Text("\(ImageName)")
+                .frame(alignment: .leading)
         }
     }
 }
